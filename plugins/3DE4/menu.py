485aa708a61ea89f65a493a076379e79456bd284
Danb106632 <daniel.beeching3@gmail.com>
2025-12-27 23:34:20 +0000

    Updated 3DE plugin with auto version and os detection

--- plugins/3DE4/menu.py
+++ plugins/3DE4/menu.py
@@ -1,14 +1,49 @@
 import nuke
 import os
 import glob
- 
-plugins_menu = nuke.menu('Nodes').addMenu("Escape Studios/3DE4", icon="3de_white.png")
- 
-dll_pattern = os.path.join(
-    os.path.dirname(__file__), 
-    "{}.{}".format(nuke.NUKE_VERSION_MAJOR, nuke.NUKE_VERSION_MINOR),
-    "*.dll"
-)
-for dll_file in glob.glob(dll_pattern):
-    plugin_name = os.path.splitext(os.path.basename(dll_file))[0]
+import platform
+
+plugins_menu = nuke.menu('Nodes').addMenu('Escape Studios/3DE4', icon = "3de_white.png")
+
+system_os = platform.system().lower()
+
+plugin_folder = ""
+
+plugin_pattern = ""
+
+if system_os == "windows":
+    plugin_folder = os.path.join(
+        os.path.dirname(__file__),
+        system_os,
+        "Nuke{}.{}".format(nuke.NUKE_VERSION_MAJOR, nuke.NUKE_VERSION_MINOR)
+    )
+    plugin_pattern = os.path.join(
+        plugin_folder,
+        "*.dll"
+    )
+elif system_os == "linux":
+    plugin_folder = os.path.join(
+        os.path.dirname(__file__),
+        system_os,
+        "Nuke{}.{}".format(nuke.NUKE_VERSION_MAJOR, nuke.NUKE_VERSION_MINOR)
+    )
+    plugin_pattern = os.path.join(
+        plugin_folder,
+        "*.so"
+    )
+elif system_os == "darwin":
+    plugin_folder = os.path.join(
+        os.path.dirname(__file__),
+        "osx",
+        "Nuke{}.{}".format(nuke.NUKE_VERSION_MAJOR, nuke.NUKE_VERSION_MINOR)
+    )
+    plugin_pattern = os.path.join(
+        plugin_folder,
+        "*.dylib"
+    )
+
+nuke.pluginAddPath(plugin_folder)
+
+for plugin_file in glob.glob(plugin_pattern):
+    plugin_name = os.path.splitext(os.path.basename(plugin_file))[0]
     plugins_menu.addCommand(plugin_name, "nuke.createNode('{}')".format(plugin_name))
