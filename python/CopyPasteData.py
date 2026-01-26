6b352433680224f597bd61b266ee7490d8dab602
Danb106632 <daniel.beeching3@gmail.com>
2026-01-26 22:33:58 +0000

    updated and refactored CopyPaste

--- python/CopyPasteData.py
+++ python/CopyPasteData.py
@@ -1,27 +1,44 @@
+import platform
 import nuke
 import os
 
-pathWindowsSMCA = r"S:/public/CopyPasteData"
-pathWindowsUG = r"U:/public/CopyPasteData"
-fileRaw = "CopyPasteData_"
+FILE_RAW = "CopyPasteData_"
+SYSTEM = platform.system()
+
+PATHS = {
+    "Windows": {
+        "SMCA": r"S:/public/CopyPasteData",
+        "UG":   r"U:/public/CopyPasteData",
+    },
+    "Linux": {
+        "SMCA": r"/escape/shares/SMCA/public/CopyPasteData",
+        "UG":   r"/escape/shares/UG/public/CopyPasteData",
+    }
+}
+
+
+def platformPath(course="UG"):
+    return PATHS.get(SYSTEM, {}).get(course)
+
+
+def _try_nuke_op(op, file):
+    for course in ("SMCA", "UG"):
+        base = platformPath(course)
+        if not base:
+            continue
+        try:
+            op(os.path.join(base, file))
+            return
+        except RuntimeError:
+            pass
+    nuke.message("File Path Not Found!")
+
 
 def CopyData(number):
-    file = fileRaw + str(number) + ".nk"
-    try:
-        nuke.nodeCopy(os.path.join(pathWindowsSMCA, file))
-    except RuntimeError:
-        try:
-            nuke.nodeCopy(os.path.join(pathWindowsUG, file))
-        except RuntimeError:
-            nuke.message("File Path Not Found!")
+    file = f"{FILE_RAW}{number}.nk"
+    _try_nuke_op(nuke.nodeCopy, file)
 
 
 def PasteData(number):
-    file = fileRaw + str(number) + ".nk"
-    try:
-        nuke.nodePaste(os.path.join(pathWindowsSMCA, file))
-    except RuntimeError:
-        try:
-            nuke.nodePaste(os.path.join(pathWindowsUG, file))
-        except RuntimeError:
-            nuke.message("File Path Not Found!")
+    file = f"{FILE_RAW}{number}.nk"
+    _try_nuke_op(nuke.nodePaste, file)
